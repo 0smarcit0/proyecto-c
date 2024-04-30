325f2a9b89d473846f578c7beb548e6bf02e5a50
0smarcit0 Porras <112773178+0smarcit0@users.noreply.github.com>
2024-04-30 12:01:44 -0400

    main del proyecto
    creando el archivo

---
+++ proyecto_compu1_c.c
@@ -0,0 +1,8 @@
+/*V 30982189 Useche Cordero Tomas Antonio
+V 31509694 Porras Chacón Osmar Leonardo 10
+V 29507510 Chacón Moncada Luis Nabid-
+Equipo 10 Computacion 1 2024-1*/
+
+
+//Proyecto 20 puntos Parcial II Lenguaje C Seccion 11
+
