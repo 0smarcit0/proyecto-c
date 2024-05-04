fb9ac16fa82d19381e1e2c9154df16faf043bb55
0smarcit0 Porras <112773178+0smarcit0@users.noreply.github.com>
2024-05-04 11:24:41 -0400

    mas cositas

--- proyecto_compu1_c.c
+++ proyecto_compu1_c.c
@@ -17,10 +17,16 @@ Equipo 10 Computacion 1 2024-1*/
 
 int main(){
     setlocale(LC_ALL,"spanish");
-    int respuestaDia, respuestaMenu, comparadorCedulaCliente, respuestaProductoCarrito, respuestaCicloProveedor;
+    int respuestaDia, respuestaMenu, comparadorCedulaCliente, respuestaProductoCarrito, respuestaCicloProveedor,respuestaMostrarCarrito;
     char nombreCliente[30],apellidoCliente[30], direccionCliente[30];
     int cedulaCliente, cantVisitasMercado=1, checker, idClienteRegistrado,checkerProveedor,checkValidezIndiceProducto=0,checkerValidezCantProducto=0;
+    float descMarcaAsociada =0;
     int seleccionProducto=0, seleccionCantProduct,seleccionProveedor;
+    float totalCompra, totalParcial;
+    int carritoCompra[50],cantProductosIngresados=0, cantProductosSeleccionados[50];
+    time_t tiempoahora;
+    time(&tiempoahora);
+    
 
     char infoClientesNombre[n][30], infoClientesApellido[n][30],infoClientesDireccion[n][30];
     int visitasMercado[n][2]; 
@@ -32,6 +38,8 @@ int main(){
     
     char proveedores[3][50]={{"Empresas Polar"},{"Paisa"}, {"Frutas y Verduras Pepito Perez"}};
     int idProveedores[3]={0,1,2};
+
+    
 
     int productos[n][4]= {{10,10,0,0},
                          {11,10,0,1},
@@ -48,9 +56,9 @@ int main(){
 
     
     do{
-        printf("\t\t ======================== \n");
+        printf("\t ======================== \n");
         printf("\t BIENVENIDO AL SUPERMERCADO!\n");
-        printf("\t\t ======================== \n");
+        printf("\t ======================== \n");
         do
         {
             printf("OPCIONES.\n");
@@ -61,6 +69,11 @@ int main(){
             switch (respuestaMenu)
             {
             case 1:
+                cantProductosIngresados=0;
+                checker =0;
+                totalCompra= 0;
+                totalParcial= 0;
+                descMarcaAsociada=0;
                 printf("\tBienvenido al carrito, primeramente ingrese los datos del cliente.\n");
                 printf("Cedula de identidad del cliente: ");
                 scanf("%d",&comparadorCedulaCliente);
@@ -78,6 +91,7 @@ int main(){
                     printf("\napellido: %s", infoClientesApellido[idClienteRegistrado]);
                     printf("\nCedula: %d", visitasMercado[idClienteRegistrado][0]);
                     printf("\nDireccion: %s", infoClientesNombre[idClienteRegistrado]);
+                    system("pause");
                 }else{
                     visitasMercado[cantVisitasMercado][0]= comparadorCedulaCliente;
                     printf("\nIngrese el nombre del cliente: ");
@@ -96,6 +110,19 @@ int main(){
                     {
                         checkerProveedor=0;
                         system("cls");
+                        printf("Desea ver el carrito actual? \n 1)si\n 2)no\n ");
+                        scanf("%d",&respuestaMostrarCarrito);
+                        if(respuestaMostrarCarrito==1){
+                            if(cantProductosIngresados ==0){
+                                printf("El carrito esta vacio. \n");
+                            }else{
+                                for(int i=0;i<cantProductosIngresados;i++){
+                                    printf("%s, Cantidad: %d \n",nombreProducto[carritoCompra[i]],cantProductosSeleccionados[i]);
+                                }
+                                printf("%f \n",totalParcial);
+                            }
+
+                        }
                         printf("Selecione uno de nuestros proveedores para ver los productos de esa marca: \n");
                         for(int i= 0;i<3;i++){
                             printf("%d) %s \n",i,proveedores[i]);
@@ -108,6 +135,9 @@ int main(){
                             if(seleccionProveedor==idProveedores[i]){
                                 checkerProveedor = 1;
                             }
+                        }
+                        if(seleccionProveedor>=0 && seleccionProveedor<=2){
+                            descMarcaAsociada = 0.1;
                         }
                         
                     } while (checkerProveedor==0);
@@ -128,15 +158,20 @@ int main(){
                             for(int i=0;i<12;i++){
                             if(seleccionProveedor==productos[i][2]){
                                 if(productos[i][3]==seleccionProducto){
-                                    checkValidezIndiceProducto=1;
+                                    if(productos[i][1]<=0){
+                                        printf("\n Ya no queda stock de este producto.\n");
+                                    }else{
+                                       checkValidezIndiceProducto=1;
+                                       carritoCompra[cantProductosIngresados] =seleccionProducto;
+                                    }
+                                    
+                                    
                                 }
                             }        
                          }
 
                         } while (checkValidezIndiceProducto==0);
-                        
-
-                        
+                    
                         do{
                             checkerValidezCantProducto=0;
                             printf("Ingrese que cantidad desea llevar: ");
@@ -146,29 +181,51 @@ int main(){
                                if(productos[seleccionProducto][1]<seleccionCantProduct){
                                   break;
                                 }else{
-
                                     productos[seleccionProducto][1]-=seleccionCantProduct;
                                     checkerValidezCantProducto =1;
+                                    cantProductosSeleccionados[cantProductosIngresados]=seleccionCantProduct;
+                                    cantProductosIngresados++;
+
                                 }
                               }        
                             }
+                            totalParcial+=(productos[seleccionProducto][0]*seleccionCantProduct);
                         } while (checkerValidezCantProducto==0);
+                        
 
                         printf("desea ingresar otro producto de este proveedor? \n 1)si \n 2)no \n");
                         scanf("%d",&respuestaCicloProveedor);
-                    
-                        
                     } while (respuestaCicloProveedor==1);
                 
                     printf("quiere agregar otro producto? ");
                     scanf("%d",&respuestaProductoCarrito);
                 } while(respuestaProductoCarrito==1);
+                
+                system("cls");
+                fflush(stdin);
+                totalCompra= totalParcial-(totalParcial*descMarcaAsociada);
+                printf("FACTURA \nDATOS DE LA FACTURA:\n");
+                  
+                struct tm *mitiempo =localtime(&tiempoahora);
+                printf("Fecha y hora de la compra: %d/%d/%d %d:%d:%d \n", mitiempo->tm_mday, mitiempo->tm_mon+1, mitiempo->tm_year+1900,mitiempo->tm_hour,mitiempo->tm_min,mitiempo->tm_sec);
+                for(int i =0;i<cantVisitasMercado;i++){
+                    if(comparadorCedulaCliente == visitasMercado[i][0]){
+                        printf("Nombre: %s \n Apellido: %s\n Cedula: %d \n Direccion: %s\n",infoClientesNombre[i],infoClientesApellido[i],comparadorCedulaCliente,infoClientesDireccion[i]);
+                    }
+                }
+                fflush(stdin);
+                for(int i=0;i<cantProductosIngresados;i++){
+                    printf("%s, Cantidad: %d\n",nombreProducto[carritoCompra[i]],cantProductosSeleccionados[i]);
+                }
+                printf("Total a pagar: %0.2f \n",totalCompra);
                 break;
+
             default:
                 break;
             }
             printf("salir o continuar?");
             scanf("%d", &respuestaMenu);
+
         } while (respuestaMenu==1);
         
 
@@ -179,5 +236,7 @@ int main(){
     getche();
     return 0;
 }
+
+
 
 
